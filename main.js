9f84e0420e5ff3d4c1e8d06a7d0493c4f271973b
Chris McGivney <cmmcgivney84@gmail.com>
2019-05-30 08:53:57 -0600

    dont know how to start the js

---
+++ main.js
@@ -0,0 +1,10 @@
+//get dom objects for calculator buttons and screen
+var buttons = document.getElementsByTagName('button');
+var output = document.getElementsByClassName('calculator_screen')
+// assigned variables
+var operator = ['x', '÷', '+', '-', '%'];
+var input = '';
+var equation =  '';
+var result = '';
+var i;
+//all buttons event listener?
